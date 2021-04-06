7e6510b5f3eab5ea7a4d7c8bc489aae76eaf419b
thyoondev <th.yoon.dev@gmail.com>
2021-04-06 11:23:51 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
-import Third from './aa/Third';
+import { useMemo, useState } from 'react';
 import './App.css';
-import Sub, { num } from './Sub';
 //1. return시에 하나의 dom만 리턴할 수 있다.
 //2. 변수선언은 let 혹은 const로만 해야함. (scope 문제)
 //3. if 사용 불가능 -> 삼항연산자 (조건 ? 값(ture) : 값(false))
@@ -12,35 +10,43 @@ import Sub, { num } from './Sub';
 // -라이브러리 사용(부트스트랩, componet-styled)
 
 function App() {
-  const [data, setData] = useState(0);
-  const [search, setSearch] = useState(0);
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [str, setStr] = useState('합계');
 
-  const download = () => {
-    //다운로드를 받고 (통신)
-    let downloadData = 5; //가정
-    setData(downloadData);
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach((i) => (sum = sum + i));
+    console.log('sum 함수 실행됨', sum);
+    return sum;
   };
-  //실행시점 :
-  //(1) App() 함수가 최초 실행될 때(App() 그림이 최초로 그려질 때)
-  //(2) 상태 변수가 변경될 때(그게 dependencyList에 등록되어 있어야함)
-  //(3) 의존리스트 관리를 할 수 있다.
-  useEffect(() => {
-    console.log('useEffect 실행됌');
-    download();
-  }, [search]);
+
+  //useMemo => 메모라이제이션(기억)
+  const addResult = useMemo(() => getAddResult(), [list]);
 
   return (
     <div>
-      Hello World!
-      <br />
-      useEffect : {num} 입니다.
-      <br /> useState : {data}입니다.
-      <button onClick={() => setData(data - 1)}>빼기</button>
-      <button onClick={() => setData(data + 1)}>더하기</button>
-      {search}
-      <button onClick={() => setSearch(2)}>검색</button>
-      <Sub />
-      <Third />
+      <button
+        onClick={() => {
+          setStr('안녕');
+        }}
+      >
+        문자 변경
+      </button>
+      <button
+        onClick={() => {
+          setList([...list, 10]);
+        }}
+      >
+        리스트 값 추가
+      </button>
+      <div>
+        {list.map((i) => (
+          <h1>{i}</h1>
+        ))}
+      </div>
+      <div>
+        {str} : {addResult}
+      </div>
     </div>
   );
 }
