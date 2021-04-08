e41310dd0c5e0ea6507398a90b59c58429063a9b
thyoondev <th.yoon.dev@gmail.com>
2021-04-08 18:50:07 +0900

    글쓰기, 글목록보기 실습(2/2)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { Route } from 'react-router';
 import './App.css';
-import Navigation from './components/Navigation';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
 
 //글쓰기, 글삭제, 글목록
 function App() {
   return (
     <div>
-      <Navigation />
-      <Route path="/" exact={true} component={ListPage} />
-      <Route path="/write" eact={true} component={WritePage} />
+      <ListPage />
     </div>
   );
 }
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const ListPage = () => {
+  const [no, setNo] = useState(6);
+
+  const [post, setPost] = useState({
+    id: '',
+    title: '',
+    content: '',
+  });
+
   const [posts, setPosts] = useState([
-    { id: 1, title: '내용1' },
-    { id: 1, title: '내용2' },
-    { id: 1, title: '내용3' },
-    { id: 1, title: '내용4' },
-    { id: 1, title: '내용5' },
-    { id: 1, title: '내용6' },
-    { id: 1, title: '내용7' },
+    { id: 1, title: '제목1', content: '내용1' },
+    { id: 2, title: '제목2', content: '내용2' },
+    { id: 3, title: '제목3', content: '내용3' },
+    { id: 4, title: '제목4', content: '내용4' },
+    { id: 5, title: '제목5', content: '내용5' },
   ]);
 
   const StyledItemBoxDiv = styled.div`
@@ -22,19 +28,47 @@ const ListPage = () => {
     align-items: center;
   `;
 
-  const StyledRemoveButton = styled.button`
-    color: red;
-  `;
+  const handleWrite = (e) => {
+    e.preventDefault(); //form태그가 하라는 액션을 중지시킴
+    //ListPage의 setPosts에 무엇을 담아야 함?
+    setPosts([...posts, post]);
+    setNo(no + 1);
+  };
+
+  const handleForm = (e) => {
+    // computed property name 문법 (키값 동적할당)
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
       <h1>리스트 페이지</h1>
+      <form onSubmit={handleWrite}>
+        <input
+          type="text"
+          placeholder="제목을 입력하세요..."
+          value={post.title}
+          onChange={handleForm}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="내용을 입력하세요..."
+          value={post.content}
+          onChange={handleForm}
+          name="content"
+        />
+        <button type="sumit">글쓰기</button>
+      </form>
       <hr />
       {posts.map((post) => (
         <StyledItemBoxDiv>
           <div>
-            번호 : {post.id} 제목 : {post.title}
+            번호 : {post.id} / 제목 : {post.title} / 내용 : {post.content}
           </div>
-          <StyledRemoveButton>삭제</StyledRemoveButton>
+          <button>삭제</button>
         </StyledItemBoxDiv>
       ))}
     </div>
